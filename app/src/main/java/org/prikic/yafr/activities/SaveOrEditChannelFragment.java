75b7ea5ec1cfb8012ec63a1bbcdf44b4c1e099ee
Borislav Prikic <borislav.prikic@appculture.com>
2016-04-17 13:34:39 +0200

    dialog fragment created in onCreateView instead of in onCreateDialog

--- app/src/main/java/org/prikic/yafr/activities/SaveOrEditChannelFragment.java
+++ app/src/main/java/org/prikic/yafr/activities/SaveOrEditChannelFragment.java
@@ -1,12 +1,15 @@
 package org.prikic.yafr.activities;
 
 import android.app.Dialog;
-import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
-import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.Button;
 import android.widget.TextView;
 
 import org.prikic.yafr.R;
@@ -21,34 +24,44 @@ public class SaveOrEditChannelFragment extends DialogFragment {
         this.fragmentTitle = fragmentTitle;
     }
 
+    @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Dialog dialog = super.onCreateDialog(savedInstanceState);
 
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        // request a window without the title
+        dialog.getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+        return dialog;
+    }
 
-        // Get the layout inflater
-        LayoutInflater inflater = getActivity().getLayoutInflater();
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        // Inflate and set the layout for the dialog
-        // Pass null as the parent view because its going in the dialog layout
-        View view = inflater.inflate(R.layout.dialog_create_or_edit_source, null);
-
-        builder.setView(view)
-                // Add action buttons
-                .setPositiveButton(R.string.save, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int id) {
-                        Timber.d("saving RSS channel");
-                    }
-                })
-                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        SaveOrEditChannelFragment.this.getDialog().cancel();
-                    }
-                });
+        View view = inflater.inflate(R.layout.dialog_create_or_edit_source, container, false);
 
         TextView title = (TextView) view.findViewById(R.id.dialog_create_or_edit_source_title);
         title.setText(fragmentTitle);
-        return builder.create();
+
+
+        Button btnCancel = (Button) view.findViewById(R.id.btnCancelSourceDialog);
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SaveOrEditChannelFragment.this.dismiss();
+            }
+        });
+
+        Button btnSave = (Button) view.findViewById(R.id.btnSaveSourceDialog);
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Timber.d("saving RSS channel");
+                SaveOrEditChannelFragment.this.dismiss();
+            }
+        });
+
+        return view;
+
     }
 }
